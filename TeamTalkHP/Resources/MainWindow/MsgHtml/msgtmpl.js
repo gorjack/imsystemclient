99e5f9957adf85c878a61d2a0635a56316850f73
gorjack <smith_ethan@aliyun.com>
2024-04-17 23:18:55 +0800

    换头像
    支持动态换头像功能（JS不会但不需要整体看一遍，有时不会的东西不一定要都看，一定有某一个点可以入手）

--- TeamTalkHP/Resources/MainWindow/MsgHtml/msgtmpl.js
+++ TeamTalkHP/Resources/MainWindow/MsgHtml/msgtmpl.js
@@ -1,18 +1,15 @@
 var external = null;
 
 String.prototype.format = function() {  
-    if(arguments.length == 0) return this;  
+    if (arguments.length == 0) return this;
     var obj = arguments[0];
-    //console.log('Logging message obj: \n', obj);
-    var s = this;  
+    var s = this;
     for (var key in obj) {
-        console.log('=============key:\n', key);
-        console.log('=============obj[key]:\n', obj[key]);
         s = s.replace(new RegExp("\\{\\{" + key + "\\}\\}", "g"), obj[key]);
+        s = s.replace(new RegExp(key, "g"), obj[key]);
     }
-
-    console.log('=============FINISH s:\n', s);
-    return s;  
+    console.log("s is %s\n", s);
+    return s;
 }  
 
 new QWebChannel(qt.webChannelTransport,
